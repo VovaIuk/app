ce8b67d022d818b46f5e6dff8dc2fc1caaa48579
Vova <examle@gmail.com>
2024-07-07 17:56:02 +0300

    First commit

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(reqest):
+    context = {
+        'name': 'Vova',
+        'title': 'Main',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True,
+        }
+    return render(reqest, 'main/index.html', context)
+
+def about(reqest):
+    return HttpResponse('About page')
+
