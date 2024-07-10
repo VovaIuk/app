08ed728f72e8781b06e5e33f14b87a6a49768cf2
Vova <examle@gmail.com>
2024-07-10 20:26:31 +0300

    add goods app, catalog, product

--- main/views.py
+++ main/views.py
@@ -1,18 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(reqest):
+def index(request):
     context = {
         'title': 'Home',
         'content': 'Магазин - HOME',
     }
-    return render(reqest, 'main/index.html', context)
+    return render(request, 'main/index.html', context)
 
-def about(reqest):
+def about(request):
     context = {
         'title': 'ABOUT',
         'content': "О нас",
         'text_on_page': 'Почему магазин хороший'
     }
-    return render(reqest, 'main/about.html', context)
+    return render(request, 'main/about.html', context)
 
