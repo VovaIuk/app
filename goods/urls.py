bef59e010530b16c4a5f5c54f92841f63e0cb222
Vova <examle@gmail.com>
2024-07-17 12:58:17 +0300

    add slug convert for product

--- goods/urls.py
+++ goods/urls.py
@@ -1,9 +1,9 @@
-﻿from django.urls import path
+from django.urls import path
 from goods import views
 
 app_name = 'goods'
 
 urlpatterns = [
     path('', views.catalog, name='index'),
-    path('product/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product'),
 ]
