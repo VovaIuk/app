d8e1ace63ddec462932170fa2226f76adaa1cc33
Vova <examle@gmail.com>
2024-07-26 16:33:13 +0300

    Connecting postgresql, adding advanced search and adding keyword highlighting

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,7 @@
+from turtle import color
 from django.db.models import Q
+from django.contrib.postgres.search import SearchHeadline, SearchVector, SearchQuery, SearchRank
+from django.utils.termcolors import background
 
 from goods.models import Products
 
@@ -8,13 +11,26 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word)>2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
 
-    q_oblects = Q()
+    result = Products.objects.annotate(rank=SearchRank(vector, query),).filter(rank__gt=0).order_by("-rank")
+    result = result.annotate(
+    headline=SearchHeadline(         
+        "name",
+        query,
+        start_sel='<span style="background-color: yellow;">',
+        stop_sel="</span>",
+        ),
+    )
 
-    for token in keywords:
-        q_oblects |= Q(description__icontains=token)
-        q_oblects |= Q(name__icontains=token)
+    result = result.annotate(
+    headline=SearchHeadline(         
+        "description",
+        query,
+        start_sel='<span style="background-color: yellow;">',
+        stop_sel="</span>",
+        ),
+    )
 
-    
-    return Products.objects.filter(q_oblects)
+    return result
